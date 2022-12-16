9bed34556809ce332401a9c851d179517a0a3fe6
parul sain <parulsain@paruls-Air.bbrouter>
2022-12-16 15:27:08 +0530

    further changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <>
         <div className="header">
             <div className="logo">
-                <img src="https://cdn.worldvectorlogo.com/logos/arts-colleges.svg" style={{height:100,width:100}} alt="" />
+                <img src="https://cdn.worldvectorlogo.com/logos/arts-colleges.svg" className="pic"  alt="" />
             </div>
             <h1>ARTS COLLEGE</h1>
             <div className="ab">
