ee78b6b01d79ea4ce23745d2d2b66a65de9e4f02
parul sain <parulsain@paruls-Air.bbrouter>
2022-12-16 13:02:58 +0530

    change of images

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,12 +7,12 @@ const posts = [
         post:"Pricipal",
     },
     {
-        image:"https://img.etimg.com/thumb/width-1200,height-900,imgsize-89700,resizemode-1,msid-39893174/news/politics-and-nation/kamla-beniwal-denies-wrongdoing-insists-she-abided-by-law.jpg",
+        image:"https://media.istockphoto.com/id/1337494696/photo/indian-man-studio-portrait.jpg?b=1&s=170667a&w=0&k=20&c=JoTquY709Lf5Hof4vaCraYThVypRbOjyCPYB96PVnqQ=",
         name:"SHRI KALRAJ MISHRA",
         post:"Chancellor",
     },
     {
-        image:"https://img.etimg.com/thumb/width-1200,height-900,imgsize-89700,resizemode-1,msid-39893174/news/politics-and-nation/kamla-beniwal-denies-wrongdoing-insists-she-abided-by-law.jpg",
+        image:"https://cdn.theconversation.com/avatars/502712/width170/image-20180830-195319-1cq8aiq.jpg",
         name:"PROF. RAJEEV JAIN",
         post:"Vice-Chancellor",
     },
@@ -23,7 +23,7 @@ const posts = [
             <div className="faculty">
             {posts.map(({post,image,name}) => (
                 <div className="ppl">
-                    <img src={image}  className="imaage" alt="" />
+                    <img src={image}  className="imaage" alt="staff" />
                     <h2>{name}</h2>
                     <p>{post}</p>
                 </div>
